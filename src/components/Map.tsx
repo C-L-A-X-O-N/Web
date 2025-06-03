3a4d6c3d0da07cf873b5a5f12082143bbc39b309
Justine Bernier <ju.bernier5@gmail.com>
2025-06-03 06:18:11 +0200

    Fix types error

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,18 +1,19 @@
-import React, { useState } from 'react';
-import { MapContainer, TileLayer, Marker, useMapEvent } from 'react-leaflet';
+import { useState } from 'react';
+import { MapContainer, TileLayer, Marker} from 'react-leaflet';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import ZoomListener from './ZoomListener';
 
 // Tableau de positions des feux
-const trafficLights = [
+const trafficLights: { id: number; position: [number, number]; label: string; }[] = [
   { id: 1, position: [47.2782063, -1.5214026], label: 'Feu 1' },
   { id: 2, position: [47.277, -1.523], label: 'Feu 2' },
   { id: 3, position: [47.279, -1.520], label: 'Feu 3' },
   { id: 4, position: [47.281, -1.518], label: 'Feu 4' },
 ];
 
-function getSvgIcon(size = 32) {
+
+function getSvgIcon(size = 32): L.Icon {
   return L.icon({
     iconUrl: 'traffic_lights.svg',
     iconSize: [size, size],
